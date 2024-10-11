b76024ccb2aad0c8240c9650f20693c8eaee50d5
Patricio Cordero Espejo <pcorderoe@gmail.com>
2024-10-10 23:51:44 -0300

    Feature/working on (#2)
    * ok
    
    * minor
    
    ---------
    
    Co-authored-by: Patricio Cordero <patricio.cordero@easycancha.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <h1 className="text-3xl">Working On!</h1>
-        <p>I'm actually working on this site, please stay on to check updates!</p>
+        <p>I am actually working on this site, please stay on to check updates!</p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
